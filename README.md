9ff8371d07c1a0f8541d39b87c15b170022ce3a0
exequielpagliari <58015928+exequielpagliari@users.noreply.github.com>
2022-07-30 23:09:19 -0300

    Agregado arrays de espacio y vehículos.

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Como método de obtener competencias dentro del ámbito del desarrollo en el len
 Se tomará en cuenta los datos de marca, patente, momento de aparcamiento y ubicación para determinar; al momento de la extracción del mismo, el monto total, tomando en cuenta la salida del rodado.
 
 Las limitaciones del programa tendran relación al nivel de conocimiento momentaneo por mi persona, ya que no se tomará en cuenta una base de datos ni un desarrollo exaustivo en la representación dentro de la programación HTML.
+
+El garaje contará con 20 espacios de vehículos.
--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 let date, horas, minutos, segundos;
+let patente, color, marca, modelo, dia, mes, año, tarifa;
+let espacios = [];
+let vehiculos = []
+let cantidadespacios = 20
+let datosdevículos = cantidadespacios * 7;
 date = new Date();
 horas = date.getUTCHours() - 3; 
 minutos = date.getUTCMinutes();
 segundos = date.getUTCSeconds();
 
+for (let index = 0; index < cantidadespacios; index++) {
+    espacios.push(0);     
+}
+
+for (let index = 0; index < datosdevículos; index++) {
+    espacios.push(0);     
+}
+
+
 
 console.log("Hora " + horas);
 console.log("Minutos " + minutos);
 console.log("Segundos "+ segundos);
+console.log(espacios)
